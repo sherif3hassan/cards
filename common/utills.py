9e5c1b32a6945bfcf19dd032eabccbe1f6264d5f
sherif <sherif3hassan@gmail.com>
2023-01-28 22:44:19 +0200

    Add get player function

--- common/utills.py
+++ common/utills.py
@@ -1,5 +1,14 @@
+from fastapi import Depends
+from auth.auth_bearer import JWTBearer
+from auth.auth_handler import decode_jwt
 def clean_dict(update: dict):
     update_dict = {
         key: value for key, value in zip(update.keys(), update.values()) if value is not None
     }
     return update_dict
+
+def get_player(token = Depends(JWTBearer)):
+    data = decode_jwt(token)
+    return data
+
+    
